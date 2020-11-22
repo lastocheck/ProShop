b8a5fdf7ed25896c51f087a8f6b212ed465c8667
Evgeniy Nefedov <lastocheck@gmail.com>
2020-11-22 12:00:31 +0300

    Use Redux to fetch products to HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
 import Product from '../Components/Product'
+import { listProducts } from '../actions/productActions'
+import { productListReducer } from '../reducers/productReducers'
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-
-            setProducts(data)
-        }
-
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
 
     return (
         <>
             <h1>Latest Products</h1>
+            {loading ? <h2>loading...</h2> : error ? <h3>{error}</h3> :
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -26,7 +26,8 @@ const HomeScreen = () => {
 
                     </Col>
                 ))}
-            </Row>
+            </Row>}
+            
         </>
     )
 }
